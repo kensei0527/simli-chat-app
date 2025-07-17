771559b7b230f8867b0eb7e98613fdc5529d3b39
kensei0527 <kensei268@icloud.com>
2025-07-18 00:05:32 +0900

    Enhance DottedFace component by adding optional className prop for improved styling flexibility.

--- src/app/Components/DottedFace.tsx
+++ src/app/Components/DottedFace.tsx
@@ -2,7 +2,7 @@
 import Image from 'next/image';
 import dottedface from '@/media/dottedface.gif';
 
-export default function DottedFace(props: React.PropsWithChildren<{}>) {
+export default function DottedFace(props: React.PropsWithChildren<{className?: string}>) {
     return (
     
         <div className="flex justify-center items-center">
